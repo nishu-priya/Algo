e947a51fd1ec6e7b6d1145afb82a332c75b216f8
nishu-priya <nishu.priya@flipkart.com>
2019-10-05 18:52:03 +0530

    Addes xcode proj

---
+++ Algos/Algos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Algos
+//
+//  Created by Nishu Priya on 05/10/19.
+//  Copyright © 2019 Nishu Priya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
